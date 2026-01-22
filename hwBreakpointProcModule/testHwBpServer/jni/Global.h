56282cdc64009ece33c07ae97fc18eda41cfc2e5
wangzhifang293-prog <wangzhifang293@gmail.com>
2026-01-23 01:22:31 +0800

    Delete hwBreakpointProcModule directory

--- hwBreakpointProcModule/testHwBpServer/jni/Global.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <map>
-#include <vector>
-#include <memory>
-#include <atomic>
-#include "api.h"
-#include "../../testHwBp/jni/HwBreakpointMgr4.h"
-extern CHwBreakpointMgr g_driver;
-
